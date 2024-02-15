a72dfa3388eca5aef2af4782a884580e5161235b
unknown <szymon.balcerzak13@gmail.com>
2024-02-15 12:05:49 +0100

    first commit

---
+++ src/main/java/com/ainotes/backendainotes/bootloader/BackendAiNotesApplication.java
@@ -0,0 +1,13 @@
+package com.ainotes.backendainotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAiNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAiNotesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ainotes/backendainotes/dto/CompletionGptRequest.java
@@ -0,0 +1,2 @@
+package com.ainotes.backendainotes.dto;public class CompletionGptRequest {
+}
