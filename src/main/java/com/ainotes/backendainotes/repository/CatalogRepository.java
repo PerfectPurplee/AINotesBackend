fd05306ca8dc8aedbc55b55222a7ca8e54b0a3f3
unknown <szymon.balcerzak13@gmail.com>
2024-02-22 12:37:49 +0100

    basic setup update for developing application on laptop

--- src/main/java/com/ainotes/backendainotes/repository/CatalogRepository.java
+++ src/main/java/com/ainotes/backendainotes/repository/CatalogRepository.java
@@ -1,7 +1,10 @@
 package com.ainotes.backendainotes.repository;
 
 import com.ainotes.backendainotes.model.Catalog;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CatalogRepository extends CrudRepository<Catalog, Long> {
+@Repository
+public interface CatalogRepository extends JpaRepository<Catalog, Long> {
 }
--- src/main/java/com/ainotes/backendainotes/repository/UserRepository.java
+++ src/main/java/com/ainotes/backendainotes/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.ainotes.backendainotes.repository;
 
 import com.ainotes.backendainotes.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
-public interface UserRepository extends CrudRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserByUsernameAndLogin(String username, String login);
 }
